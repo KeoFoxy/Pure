af552ff2606917bc33a87d236ac080158d7198f9
KeoFoxy <arasdiel68@gmail.com>
2022-06-08 13:39:05 +0300

    app init

---
+++ PureMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureMagicApp.swift
+//  PureMagic
+//
+//  Created by Alexander Sorokin on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PureMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
